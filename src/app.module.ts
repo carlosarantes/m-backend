f633d7e7baa4662d523898131c39052389ae2723
Carlos Augusto Arantes <carlos.arantes@bolttech.io>
2022-09-13 23:26:59 +0100

    WIP - image report

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ImageReportsModule } from './image_reports/image_reports.module';
@@ -8,6 +10,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'upload'),
+    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
     UsersModule,
